9edc64029d236b823c110b98607016bf94d9ff7c
Max <maxhuang4242@gmail.com>
2025-02-10 19:10:19 +1100

    initialised firestore

--- config/firebase/db_config.js
+++ config/firebase/db_config.js
@@ -1,3 +1,18 @@
-export const DB_CONFIG = {
-    
-}
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBz4tN7zvbTQb3JBu3ROu7Hp-ePMrHbb64",
+    authDomain: "flashcardapp-7f4f8.firebaseapp.com",
+    projectId: "flashcardapp-7f4f8",
+    storageBucket: "flashcardapp-7f4f8.firebasestorage.app",
+    messagingSenderId: "104377682313",
+    appId: "1:104377682313:web:1784967e925ecaacbfb958",
+    measurementId: "G-Z17FPLCQ1H"
+};
+
+const app = !firebase.apps.length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { db };
